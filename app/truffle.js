28557b0ebe3dff05c223d8e7b5497a0ebf48470c
Todd Schultz <toddschultz@pop-os.localdomain>
2018-11-14 12:50:56 -0500

    config for testnet

--- app/truffle.js
+++ app/truffle.js
@@ -2,8 +2,18 @@ module.exports = {
   networks: {
     development: {
       host: "localhost",
-      port: 7545,
+      port: 8545,
       network_id: "*" // Match any network id
-    }
-  }
+    },
+     ropsten:  {
+     network_id: 3,
+     host: "localhost",
+     port:  8545,
+     gas:   2900000
+}
+  },
+   rpc: {
+ host: 'localhost',
+ post:8080
+   }
 };
